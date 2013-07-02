bd83f5e463e3c0073a7694b976dc2f1070dad58d
André Walker <andre@andrewalker.net>
2013-07-02 02:49:55 -0300

    Save and display comments in guestbook

--- guestbook.go
+++ guestbook.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "reflect"
     "fmt"
     "net/http"
     "text/template"
@@ -17,25 +16,60 @@ type Login struct {
     Password string
 }
 
-type Comment struct {
-    Username string
-    Comment string
+type GuestbookRow struct {
+    Id          int
+    User        string
+    Written_at  string
+    Comment     string
+}
+
+type GuestbookRows struct {
+    Rows []GuestbookRow
 }
 
 var db *sql.DB
+
 var decoder       = schema.NewDecoder()
 var store         = sessions.NewCookieStore([]byte("frase-ultra-secreta-de-encriptacao"))
+
 var homeTemplate  = template.Must(template.New("home").ParseFiles("templates/home.html"))
 var loginTemplate = template.Must(template.New("login").ParseFiles("templates/login.html"))
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
     session, _ := store.Get(r, "auth-session")
-    username := session.Values["username"]
-    data := make(map[string]interface{})
-    if reflect.TypeOf(username).String() == "string" {
+
+    data := map[string]interface{} {
+        "logged_in": false,
+        "username": "",
+    }
+
+    if username, ok := session.Values["username"]; ok {
         data["logged_in"] = true
         data["username"] = username
     }
+
+    rows, err := db.Query("SELECT id, username, to_char(written_at, 'DD/MM/YYYY às HH24:MI'), comment FROM guestbook")
+
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        guestbook_rows := new(GuestbookRows)
+
+        for rows.Next() {
+            var id int
+            var user string
+            var written_at string
+            var comment string
+
+            rows.Scan(&id, &user, &written_at, &comment)
+
+            newRow := GuestbookRow{id,user,written_at,comment}
+            guestbook_rows.Rows = append(guestbook_rows.Rows, newRow)
+        }
+
+        data["rows"] = guestbook_rows
+    }
+
     if err := homeTemplate.ExecuteTemplate(w, "home", data); err != nil {
         fmt.Println(err);
     }
@@ -53,18 +87,18 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func DoLoginHandler(w http.ResponseWriter, r *http.Request) {
-    var password string
-    page := "/login"
+    var count int
+    page := "/"
     login := new(Login)
     session, _ := store.Get(r, "auth-session")
 
     r.ParseForm();
     decoder.Decode(login, r.PostForm)
 
-    err := db.QueryRow("SELECT password FROM users WHERE username=?", login.Username).Scan(&password)
+    db.QueryRow("SELECT COUNT(*) FROM users WHERE username=$1 AND password=$2", login.Username, login.Password).Scan(&count)
 
-    if err == sql.ErrNoRows {
-        page = page + "?wrong_password=1"
+    if count == 0 {
+        page = "/login?wrong_password=1"
     } else {
         session.Values["username"] = login.Username
     }
@@ -75,6 +109,23 @@ func DoLoginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GuestbookHandler(w http.ResponseWriter, r *http.Request) {
+    session, _ := store.Get(r, "auth-session")
+
+    if username, ok := session.Values["username"]; ok {
+        tx, _ := db.Begin()
+        stmt, err := db.Prepare("INSERT INTO guestbook (username, comment) VALUES ( $1, $2 )")
+        if err != nil {
+            fmt.Println(err)
+        } else {
+            if comment := r.FormValue("comment"); comment != "" {
+                stmt.Exec(username, comment)
+                tx.Commit()
+            } else {
+                tx.Rollback()
+            }
+        }
+    }
+
     // após gravar no guestbook, redirecione
     http.Redirect(w, r, "/", http.StatusFound)
 }
@@ -87,7 +138,7 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     var err error
-    db, err = sql.Open("postgres", "user=andre dbname=guestbook")
+    db, err = sql.Open("postgres", "user=andre dbname=guestbook sslmode=disable")
 
     if err != nil {
         fmt.Println(err)
