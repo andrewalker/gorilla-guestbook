933d3abcf15cc6d44eab1a0b445defaff7362a70
André Walker <andre@andrewalker.net>
2013-07-01 22:59:06 -0300

    Implement silly login routine

--- guestbook.go
+++ guestbook.go
@@ -5,18 +5,20 @@ import (
     "net/http"
     "text/template"
     "github.com/gorilla/mux"
+    "github.com/gorilla/schema"
 )
 
 type Login struct {
-    user string
-    password string
+    Username string
+    Password string
 }
 
 type Comment struct {
-    user string
-    comment string
+    Username string
+    Comment string
 }
 
+var decoder       = schema.NewDecoder()
 var homeTemplate  = template.Must(template.New("home").ParseFiles("templates/home.html"))
 var loginTemplate = template.Must(template.New("login").ParseFiles("templates/login.html"))
 
@@ -30,6 +32,39 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
     if err := loginTemplate.ExecuteTemplate(w, "login", nil); err != nil {
         fmt.Println(err);
     }
+}
+
+func DoLoginHandler(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm();
+    login := new(Login)
+    decoder.Decode(login, r.PostForm)
+
+    switch login.Username {
+        case "andre": {
+            if login.Password == "123" {
+                fmt.Println("Usuário é André.");
+            } else {
+                fmt.Println("Senha errada.");
+            }
+        }
+        case "pedro": {
+            if login.Password == "123" {
+                fmt.Println("Usuário é Pedro.");
+            } else {
+                fmt.Println("Senha errada.");
+            }
+        }
+        case "lucas": {
+            if login.Password == "123" {
+                fmt.Println("Usuário é Lucas.");
+            } else {
+                fmt.Println("Senha errada.");
+            }
+        }
+        default: fmt.Println("O usuário não existe.");
+    }
+
+    http.Redirect(w, r, "/login", http.StatusFound)
 }
 
 func GuestbookHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +83,7 @@ func main() {
     r.HandleFunc("/",          HomeHandler)
     r.HandleFunc("/guestbook", GuestbookHandler)
     r.HandleFunc("/login",     LoginHandler)
+    r.HandleFunc("/do_login",  DoLoginHandler)
     r.HandleFunc("/logout",    LogoutHandler)
 
     http.Handle("/", r)
